0d5a96c3bf4cafc127357c43720a1efbe2a0e0b3
Paulo Amaro <paulogabriel.amaro@gmail.com>
2015-08-27 09:41:03 -0300

    delete

--- src/br/com/evo/modelo/dao/newSQLTemplate.sql
+++
@@ -1,6 +0,0 @@
-insert into FND_Pessoas(nome_completo,nome_primeiro,nome_ultimo,email,telefone_com,
-telefone_cel,telefone_fax,empresa,empresa_id,pais,estado,cidade,login,password,area_nome,
-area_id) 
-VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,MD5(?),?,?);
-
-SELECT * FROM FND_Pessoas WHERE RequestID = ?;
